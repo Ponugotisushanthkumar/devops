91e5c3711a93b29266c5b35702572d6c45e8e39f
nikhila611 <nikhilanikki611@gmail.com>
2026-04-22 18:01:24 -0500

    Add project files including Terraform, Jenkins, and scripts

---
+++ jenkins.tf
@@ -0,0 +1,5 @@
+apt update
+apt install -y wget unzip
+wget https://releases.hashicorp.com/terraform/1.6.6/terraform_1.6.6_linux_amd64.zip
+unzip terraform_1.6.6_linux_amd64.zip
+mv terraform /usr/local/bin/
---
+++ sunny.py
@@ -0,0 +1,5 @@
+count = 0
+for numbers in range(1, 10):
+    if numbers % 2 == 0:
+        print(numbers)
+        print(f"we have{count} of even numbers")
